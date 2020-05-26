e02d0babf0e57730b4aafa1f357e8f9ddf6507c1
itsnmk <noormd786khan@gmail.com>
2020-05-26 15:37:30 +0000

    issue solved

--- include/processor.h
+++ include/processor.h
@@ -3,13 +3,14 @@
 
 class Processor {
  public:
- Processor();
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
- long activeJiffies_{0};
-long totalJiffies_{0};
+  long prevIdle = 0;
+  long prevNonIdle = 0;
+  long Idle;
+  long NonIdle;
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,22 +1,19 @@
-#include "processor.h"
-#include<vector>
-#include<iostream>
-using std::vector;
-#include "linux_parser.h"
 
-Processor::Processor() {
- Processor::Utilization();
-}
+#include "processor.h"
+
+#include "linux_parser.h"
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() {
-  vector<long> active{activeJiffies_, LinuxParser::ActiveJiffies()};
-  vector<long> total{totalJiffies_, LinuxParser::Jiffies()};
-  float util = 1.0 * (active[1] - active[0]) / (total[1] - total[0]);
-  activeJiffies_ = active[1];
-  totalJiffies_ = total[1];
-  return util;
+  Idle = LinuxParser::IdleJiffies();
+  NonIdle = LinuxParser::ActiveJiffies();
+  long prevTotal = prevIdle + prevNonIdle;
+  long Total = Idle + NonIdle;
+
+  long TotalDelta = Total - prevTotal;
+  long IdleDelta = Idle - prevIdle;
+  prevIdle = Idle;
+  prevNonIdle = NonIdle;
+  return (float)(TotalDelta - IdleDelta) / TotalDelta;
 }
-
-
 
 
